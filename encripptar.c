e1e9ef0d807432ee764534bcc914244b801f9410
Walter <wallepro39@gmail.com>
2024-10-21 12:11:38 -0600

    Update encripptar.c

--- encripptar.c
+++ encripptar.c
@@ -1,20 +1,44 @@
 /******************************************************************************
 
-encriptar frase 
+encriptar avvbeecedario
 
-*******************************************************************************/
+***************************************************************/
 #include <stdio.h>
+void encriptar (char frase[100], char abc[30]);
 
 int main()
 {   
+    //char abc1[]="abcdefghijklmnopqrstuvwxyz";
+    char abc2[]="%@c&wXo!yz*j@vBL7q#I_3Kdpen";
     char frase[200]="Guadalajara es la perla Tapatía";
+    
+    encriptar(frase, abc2);
+    printf("%s\n", frase);
+    desencriptar(frase,abc2);
+    printf("%s", frase);
+    
+    return 0;
+}
+
+void desencriptar (char frase[100], char abc[30])
+{
     for (int pos=0;frase[pos]!='\0' ;pos++)
     {
-        if (frase [pos]==' ')// if (frase[pos]>='a'&&frase[pos]<='Z');
-            continue;
-        frase[pos]=frase[pos]+1;
-    }
-    printf("%c", frase [0]);
+        int letra=frase[pos];
+        for (int lugar=0;abc[lugar]!='\0' ;lugar++)
+        {
+            if (abc[lugar]==letra )
+                frase[pos]=lugar+97;
+        }
+    } 
+}
 
-    return 0;
+void encriptar (char frase[100], char abc[30])
+{
+    for (int pos=0;frase[pos]!='\0' ;pos++)
+    {
+        int resta=frase[pos]-97;
+        if (frase [pos]>='a' && frase[pos]<='z')
+            frase[pos]=abc[resta]; //frase[pos]+1
+    } 
 }
